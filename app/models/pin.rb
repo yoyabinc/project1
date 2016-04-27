349241119c3803ec581ca8486f4fbaa9d31c876c
Sohrab Sheikhha <s.sheikhha@gmail.com>
2016-04-27 16:41:36 -0700

    Finally pictures are done

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-	Paperclip.options[:content_type_mappings] = { jpg: "image/jpeg" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
