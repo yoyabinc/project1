522e26c5e034789d8cd1993405158a4e42cab1aa
Sohrab Sheikhha <s.sheikhha@gmail.com>
2016-04-27 13:21:58 -0700

    cleaning up the pins_controller file

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,28 +1,20 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /pins
-  # GET /pins.json
   def index
     @pins = Pin.all
   end
 
-  # GET /pins/1
-  # GET /pins/1.json
   def show
   end
 
-  # GET /pins/new
   def new
     @pin = Pin.new
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
-  # POST /pins.json
   def create
     @pin = Pin.new(pin_params)
 
@@ -36,9 +28,6 @@ class PinsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
   def update
     respond_to do |format|
       if @pin.update(pin_params)
@@ -51,8 +40,6 @@ class PinsController < ApplicationController
     end
   end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
   def destroy
     @pin.destroy
     respond_to do |format|
@@ -62,12 +49,10 @@ class PinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
     end
